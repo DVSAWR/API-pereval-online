8b1a75cb898e1557d27e84734b812e000a79ac32
DVSAWR <126908465+DVSAWR@users.noreply.github.com>
2024-02-05 14:04:20 +0300

    add

--- API_pereval_online/app_main/admin.py
+++ API_pereval_online/app_main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser, Coords, Post, Images
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Coords)
+admin.site.register(Post)
+admin.site.register(Images)
--- API_pereval_online/app_main/models.py
+++ API_pereval_online/app_main/models.py
@@ -1,17 +1,52 @@
+from django.contrib.auth.models import User
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+
 
 class CustomUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    surname = models.CharField(max_length=64, default=None)
+    fam = models.CharField(max_length=64, default=None)
     name = models.CharField(max_length=64, default=None)
-    patronymic = models.CharField(max_length=64, default=None)
+    otc = models.CharField(max_length=64, default=None)
     email = models.EmailField(unique=True, default=None)
     phone = models.CharField(max_length=11)
 
     def __str__(self):
-        return f'{self.surname} {self.name} {self.patronymic}'
+        return f'{self.fam} {self.name} {self.otc}'
 
 
 class Coords(models.Model):
-    ...
+    latitude = models.FloatField(max_length=254)
+    longitude = models.FloatField(max_length=254)
+    height = models.IntegerField(max_length=254)
+
+    def __str__(self):
+        return f'{self.latitude} {self.longitude} {self.height}'
+
+
+class Post(models.Model):
+    NEW, PENDING, ACCEPTED, REJECTED = 'N', 'P', 'A', 'R'
+    STATUS_CHOICES = [(NEW, 'New'), (PENDING, 'Pending',), (ACCEPTED, 'Accepted',), (REJECTED, 'Rejected')]
+
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=NEW)
+    beauty_title = models.CharField(max_length=254)
+    title = models.CharField(max_length=254)
+    other_titles = models.CharField(max_length=254)
+    connect = models.TextField(blank=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    level_spring = models.CharField(max_length=64, blank=True)
+    level_summer = models.CharField(max_length=64, blank=True)
+    level_autumn = models.CharField(max_length=64, blank=True)
+    level_winter = models.CharField(max_length=64, blank=True)
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='pereval')
+    coords = models.OneToOneField(Coords, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.pk} {self.title}'
+
+
+class Images(models.Model):
+    title = models.CharField(max_length=254)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+    date_added = models.DateField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
